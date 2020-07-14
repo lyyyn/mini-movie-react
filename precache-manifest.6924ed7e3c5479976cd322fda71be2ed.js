ff1a60e7f278bcde3fe3a2c8128815855f6e4545
Linawaty <linawatylim@gmail.com>
2020-07-14 20:38:35 +0800

    Updates

---
+++ precache-manifest.6924ed7e3c5479976cd322fda71be2ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7daad5567788bd36d54ccb8210b4ed",
+    "url": "/mini-movie-react/index.html"
+  },
+  {
+    "revision": "16fe98b96686336f60c5",
+    "url": "/mini-movie-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5983cbe4573e7b004975",
+    "url": "/mini-movie-react/static/js/2.32982e54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mini-movie-react/static/js/2.32982e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16fe98b96686336f60c5",
+    "url": "/mini-movie-react/static/js/main.c8d41a83.chunk.js"
+  },
+  {
+    "revision": "0035b2a63aae0cabbf62",
+    "url": "/mini-movie-react/static/js/runtime-main.c5590d58.js"
+  }
+]);
